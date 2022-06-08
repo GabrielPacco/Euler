35318b1c165861d22c1dfb39e64bc53dd4c62c1d
GabrielPacco <gpaccohu@unsa.edu.pe>
2022-06-08 14:07:40 -0500

    Corrigiendo el Metodo de Euler desarrollado en clase

--- Euler3.py
+++ Euler3.py
@@ -3,6 +3,7 @@
 # utilizando el método de Euler.
 from turtle import end_fill
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 
 def func( y, t ):
@@ -15,25 +16,25 @@ def Y(t):
 a=0
 b=2
 c=1
-n=10
+n=100
 
-h=(b-a)/(n-1)
-#t=np.zeros(1,n)
-#y=np.zeros(1,n)
-y:(1)=c
-t:(1)=a
+h= (b-a)/(n-1)
+t=np.zeros((n))
+y=np.zeros((n))
+y[0]=c
+t[0]=a
 # Valor de x al que necesitamos aproximación
 
-for i in range(2,n):
-	t:(i)=t[i-1]+h
-	t:(i)=y[i-1]+h*func(y[i-1],t[i-1])
+for i in np.arange(1,n):
+	t[i]=t[i-1]+h
+	y[i]=y[i-1]+func(y[i-1],t[i-1])*h
+	
 
-def graficar(x, y):
-	plt.plot(x, y, 'r')
-	plt.xlabel('x')
-	plt.ylabel('y')
-	plt.title('Euler')
-	plt.show()
-
-graficar(t, y)
-
+# Graficando
+plt.grid(True)
+plt.plot(t,y, '*')
+plt.plot(t,Y(t), 'r')
+#tt = np.linspace(a,0.1,b)
+#yy = Y(tt)
+#plt.plot(tt,yy, 'r')
+plt.show()
